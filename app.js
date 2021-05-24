32ef1fd5c7a4524d82a2526199525f7940dc31c7
JuanSegundo7 <juansepincha@gmail.com>
2021-05-24 16:37:42 -0300

    avanzando en el proyecto de ml

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ app.listen(3000, () => console.log("Server star in https://localhost:3000jajaj")
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./views", "home.html"))
 );
+
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views", "register.html"))
+);
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views", "login.html"))
+);
