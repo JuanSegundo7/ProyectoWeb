a0bb628e694c3fb780e3068e4ad5ba763bc62a40
JuanSegundo7 <juansepincha@gmail.com>
2021-05-27 19:27:38 -0300

    actualizado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-app.listen(3000, () => console.log("Server star in https://localhost:3000jajaj"));
+app.listen(3000, () => console.log("Server starts in https://localhost:3000jajaj"));
 
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./views", "home.html"))
