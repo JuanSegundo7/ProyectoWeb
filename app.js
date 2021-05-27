efbd619c6f468fceccd9fa0f19100adff9e699be
JuanSegundo7 <juansepincha@gmail.com>
2021-05-27 20:05:26 -0300

    actualizacion2

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.set("port",process.env.PORT || 3000)
+
+app.listen(app.get("port"), () => console.log("Start Server"))
+
 app.use(express.static(path.resolve(__dirname, "./public")));
-app.listen(3000, () => console.log("Server starts in https://localhost:3000jajaj"));
+app.listen(3000, () => console.log("Server starts in https://localhost:3000"));
 
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./views", "home.html"))
